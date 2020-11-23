edb9455640d4e45b4934a75a68d8032eb40e4c8e
PatriciaLai <65748020+PatriciaLai@users.noreply.github.com>
2020-11-22 23:11:17 -0500

    Fix the white box in homepage

--- final_project/js/mapVis.js
+++ final_project/js/mapVis.js
@@ -94,11 +94,11 @@ class MapVis {
         vis.legendScale = d3.scaleLinear()
             .range([0, 150])
 
-        // == TOOLTIP ==
-        // Append tooltip
-        vis.tooltip = d3.select("body").append('div')
-            .attr('class', "tooltip")
-            .attr('id', 'pieTooltip')
+        // // == TOOLTIP ==
+        // // Append tooltip
+        // vis.tooltip = d3.select("body").append('div')
+        //     .attr('class', "tooltip")
+        //     .attr('id', 'pieTooltip')
 
 
         vis.wrangleData()
